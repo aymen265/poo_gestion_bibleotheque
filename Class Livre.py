fba880036bfebc76679a03930e008a50a6cd5a7a
Aymen Lahlia <aymanehilya@gmail.com>
2025-06-16 11:21:58 +0100

    Update Class Livre.py

--- Class Livre.py
+++ Class Livre.py
@@ -23,32 +23,6 @@ class Livre:
             print(f"Le livre {self.titre} a été retourné.")
         else:
             print(f"le livre {self.titre}est dejà disponible.")
-# # new_livre = Livre("Le Petit Prince", "Antoine de Saint-Exupéry", "978-3-16-148410-0")
-# # new_livre.emprunter()
-# # new_livre.afficher_info()
-# # new_livre.emprunter()
-# # new_livre.retourne()
-# class Utilisateur(Livre):
-#     def __init__(self,id,nom,livres_empruntes,historique_emprunts):
-#         self.id = id
-#         self.nom = nom
-#         self.livres_empruntes = livres_empruntes # Liste de livres empruntés
-#         self.historique_emprunts = historique_emprunts
-#         self.penalise = False
-#         super().__init__(titre=None, auteur=None, isbn=None)  # Appel du constructeur de Livre
-
-        
-            
-
-
-#     def retourner_livre(self,isbn,bibliotheque):
-#       deadline = timedelta(days=7)
-#       if date.today()- date_emprunt > deadline:
-#             self.penalise = True
-#             print(f"L'utilisateur {self.nom} est pénalisé pour le livre {isbn}.")
-
-#     def afficher_emprunts(self):
-
 
 
 #     def afficher_hisotoire(self):
